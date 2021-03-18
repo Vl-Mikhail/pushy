158848d9e5544af43938843eb8831a5d6d86f86b
miguel <miguel@deeplay.io>
2021-03-18 22:26:31 +1000

    Add Pushy

---
+++ public/service-worker.js
@@ -0,0 +1,3 @@
+// Import Pushy Service Worker 1.0.7
+// import 'https://sdk.pushy.me/web/1.0.7/pushy-service-worker.js';
+importScripts('https://sdk.pushy.me/web/1.0.7/pushy-service-worker.js');
--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Pushy from 'pushy-sdk-web';
+
+
+// Register visitor's browser for push notifications
+Pushy.register({ appId: '5ff2c5eda21edfca797d297c' }).then(function (deviceToken) {
+    // Print device token to console
+    console.log('Pushy device token: ' + deviceToken);
+
+    // Send the token to your backend server via an HTTP GET request
+    //fetch('https://your.api.hostname/register/device?token=' + deviceToken);
+
+    // Succeeded, optionally do something to alert the user
+}).catch(function (err) {
+    // Handle registration errors
+    console.error(err);
+});
+
+// Handle push notifications (only when web page is open)
+Pushy.setNotificationListener(function (data) {
+    // Print notification payload data
+    console.log('Received notification: ' + JSON.stringify(data));
+
+    // Attempt to extract the "message" property from the payload: {"message":"Hello World!"}
+    let message = data.message || 'Test notification';
+
+    // Display an alert with message sent from server
+    alert('Received notification: ' + message);
+});
 
 ReactDOM.render(
   <React.StrictMode>
